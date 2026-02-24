de70d6c561f84e6fd787d4bf1eb53c45a8bd49ff
MayankVishwakarmaDev <153093498+MayankVishwakarmaDev@users.noreply.github.com>
2026-02-24 12:01:33 +0530

    Set basePath and assetPrefix for portfolio repo

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const repoName = "portfolio";
+
 const nextConfig = {
   output: "export",
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
   typescript: {
     ignoreBuildErrors: true,
   },
